e223650b018e25a51098cc066b91a47ffce9d193
haha <390519764@qq.com>
2022-07-28 01:45:40 +0800

    feat: 按钮增加size选项

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,14 +1,24 @@
 import React from "react"
+import classnames from 'classnames'
 import './index.scss'
 
 interface ButtonProps {
-    type?: String;
+    type?: 'default' | 'primary' | 'danger' | 'success' | 'warning'
     children?: React.ReactNode
+    size?: 'full' | 'normal' | 'small' | 'mini'
+    className?: string
 }
 
 const Button:React.FC<ButtonProps> = (props) => {
-    const { type, children } = props
-    return (<button className={`fc-button fc-button-normal fc-button-${type}`}>{children}</button>)
+    const { type, size, children, className } = props
+    const classNames = classnames(
+        'fc-button',
+        {
+            [`fc-button-${type ? type : 'default'}`]: true,
+            [`fc-button-${size ? size : 'normal'}`]: true,
+            [`${className? className: ''}`]: true
+        })
+    return (<button className={classNames}>{children}</button>)
 }
 
 export default Button
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import { Nav, Button } from '../components'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <Button type="error">{123}</Button>
+      <div>按钮类型</div>
+      <Button className="demo-button" type="default">默认按钮</Button>
+      <Button className="demo-button" type="danger">错误按钮</Button>
+      <Button className="demo-button" type="success">成功按钮</Button>
+      <Button className="demo-button" type="warning">警告按钮</Button>
+      {/* <Button size="mini">mini button</Button> */}
+      <div>按钮尺寸</div>
+      <Button className="demo-button" type="primary" size="full">normal button</Button>
+      <Button className="demo-button" type="primary" size="normal">normal button</Button>
+      <Button className="demo-button" type="primary" size="small">small button</Button>
+      <Button className="demo-button" type="primary" size="mini">mini button</Button>
       <Nav navData={[{text: 1}, {text: 2}, {text: 3}]}/>
     </div>
   )
